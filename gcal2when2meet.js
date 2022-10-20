252d5a000335f40e979f033d852f9d50a72d16db
Nezar Abdennur <nabdennur@gmail.com>
2022-10-19 21:01:27 -0400

    Update gcal2when2meet.js

--- gcal2when2meet.js
+++ gcal2when2meet.js
@@ -1,128 +1,72 @@
 (function () {
 
-document.body.appendChild(document.createElement('script')).src = 
-  "https://code.jquery.com/jquery-1.9.0.min.js";
-document.body.appendChild(document.createElement('script')).src =
-  "https://apis.google.com/js/client.js?onload=GCAL";
+document.body.appendChild(document.createElement('script')).src = "https://accounts.google.com/gsi/client";
+document.body.appendChild(document.createElement('script')).src = "https://apis.google.com/js/client.js?onload=GCAL";
 
-// when2meet
 var CLIENT_ID = "296109782810-eul22gnapke0rrf4glt74mqpk7trdjov.apps.googleusercontent.com";
 var API_KEY = "AIzaSyBHvdAgKKGarEzuA3-n0KNuiSpny8z9hbA";
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-var events = [];
 var calendars = [];
+var errors = [];
 
 function load() {
-  console.log("load");
-  gapi.load('client:auth2', initClient);
-  
-  gapi.client.setApiKey(API_KEY);
-  
-  {
-      reqCalendarList().then(function (calendars) {
-        calendars = calendars.filter(function (c) { return c.selected; });
-        return whenArray(calendars.map(reqEvents));
-      }).done(function (events) {
-        events = events.filter(function (es) { return es; });
-        selectAll();
-        if (events.length === 0) {
-          alert("Didn't find any events in this time period." +
-                " Note that when2meets that use days of the week instead of" +
-                " specific dates are not yet supported.");
-        } else {
-          console.log("events", flatten(events));
-          flatten(events).forEach(deselectEvent);
-        }
-      });
-    }
-}
-  
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
+  console.log("gcalw2m::load");
+  client = google.accounts.oauth2.initCodeClient({
+    client_id: CLIENT_ID,
     scope: SCOPES,
-    plugin_name: "foo"
-  }).then(function () {
-    // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    ux_mode: 'popup',
+    callback: main,
+  });
+  client.requestCode();
+  gapi.client.setApiKey(API_KEY);
+}
 
-    // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    
+function main() {
+  reqCalendarList().then(function (calendars) {
+    calendars = calendars.filter(function (c) { return c.selected; });
+    return Promise.all(calendars.map(reqEvents));
+  }).then(function (events) {
+    events = events.filter(function (es) { return es; });
+    selectAll();
+    if (events.length === 0) {
+      alert("Didn't find any events in this time period." +
+            " Note that when2meets that use days of the week instead of" +
+            " specific dates are not yet supported.");
+    } else {
+      // console.log("events", flatten(events));
+      flatten(events).forEach(deselectEvent);
+    }
   });
 }
-
-function updateSigninStatus(isSignedIn) {
-  console.log("Sign In Status: " + isSignedIn);
-}  
 
 function reqCalendarList() {
-  var deferred = $.Deferred();
-
-  gapi.client.calendar.calendarList.list().execute(function (res) {
-    console.log(res);
-    if (res.code === 401) {
-      gapi.auth.authorize({
-        client_id: CLIENT_ID,
-        scope: SCOPES
-      }, function () {
-        reqCalendarList().then(deferred.resolve);
-      });
-    } else {
-      console.log("authorized!");
-      deferred.resolve(res.items);
-    }
+  return gapi.client.calendar.calendarList.list().then(function (response) {
+    console.log("gcalw2m::calendar list authorized!");
+    // console.log(response.result);
+    return Promise.resolve(response.result.items);
+  }, function(response) {
+    console.log("Error fetching calendar list!");
   });
-
-  return deferred.promise();
 }
 
 function reqEvents(calendar) {
-  var deferred = $.Deferred();
-
-  gapi.client.calendar.events.list({
+  return gapi.client.calendar.events.list({
     calendarId: calendar.id,
     singleEvents: true, // expand recurring events
     timeMin: new Date(TimeOfSlot[0] * 1000).toISOString(),
     timeMax: new Date(TimeOfSlot[TimeOfSlot.length-1] * 1000).toISOString()
-  }).execute(function (res) {
-    events.push(res);
-    console.log(res);
-    deferred.resolve(res.items);
+  }).then(function (response) {
+    // console.log(response);
+    return Promise.resolve(response.result.items);
   });
-
-  return deferred.promise();
 }
-
-var errors = [];
 
 function deselectEvent(event) {
   try {
     var startTime = convertTime(event.start.dateTime);
     var endTime = convertTime(event.end.dateTime) - 900;
-    
-    while (TimeOfSlot.indexOf(startTime) == -1) {
-    	startTime += 15 * 60;
-    	if (startTime >= endTime) {
-    		return;
-    	}
-    }
-
-    while (TimeOfSlot.indexOf(endTime) == -1) {
-    	endTime -= 15 * 60;
-    	if (endTime <= startTime) {
-    		return;
-    	}
-    }
-
-    console.log("S:" + startTime + " E:" + endTime);
+    // console.log("S:" + startTime + " E:" + endTime);
     toggleRange(startTime, endTime, false);
   } catch (e) {
     errors.push(e);
@@ -141,11 +85,11 @@ function toggleRange(startTime, endTime, makeAvailable) {
     SelectStop();
   } catch (e) {
     errors.push(e);
-    console.log(e);
+    // console.log(e);
     try {
       logTime(startTime, endTime);
     } catch (e2) {
-      console.log(e2);
+      // console.log(e2);
     }
   }
 }
@@ -153,12 +97,6 @@ function toggleRange(startTime, endTime, makeAvailable) {
 function flatten(arrs) {
   // reduce was overridden by Prototype.js so use reduceRight
   return arrs.reduceRight(function (a1, a2) { return a1.concat(a2); });
-}
-
-function whenArray(promiseArr) {
-  return $.when.apply($, promiseArr).then(function () {
-    return Array.prototype.slice.call(arguments);
-  });
 }
 
 function convertTime(gcalTime) {
@@ -170,11 +108,9 @@ function convertTime(gcalTime) {
     var h = d.getMinutes() > 45 ? d.getHours() + 1 : d.getHours();
     d.setMinutes(m);
     d.setHours(h);
-    d.setSeconds(0);
   }
-  console.log("Rounded to nearest 15", gcalTime, d)
   return d.getTime() / 1000;
-  
+
 }
 
 window.SelectFromHereByTouch = function SelectFromHereByTouch(event) {
@@ -186,20 +122,19 @@ window.SelectToHereByTouch = function SelectToHereByTouch(event) {
 }
 
 function logTime(start, stop) {
-	triggerMouseEvent (document.getElementById('YouTime' + start), "touchstart");
-	triggerMouseEvent (document.getElementById('YouTime' + start), "touchmove");
-	triggerMouseEvent (document.getElementById('YouTime' + stop), "touchmove");
-	triggerMouseEvent (document.getElementById('YouTime' + stop), "touchend");
+  triggerMouseEvent(document.getElementById('YouTime' + start), "touchstart");
+  triggerMouseEvent(document.getElementById('YouTime' + start), "touchmove");
+  triggerMouseEvent(document.getElementById('YouTime' + stop), "touchmove");
+  triggerMouseEvent(document.getElementById('YouTime' + stop), "touchend");
 }
 
 function triggerMouseEvent (node, eventType) {
-    var clickEvent = document.createEvent ('MouseEvents');
-    clickEvent.initEvent (eventType, true, true);
-    node.dispatchEvent (clickEvent);
+  var clickEvent = document.createEvent ('MouseEvents');
+  clickEvent.initEvent (eventType, true, true);
+  node.dispatchEvent (clickEvent);
 }
 
 window.GCAL = load;
 window.GCAL.errors = errors;
-window.GCAL.events = events;
 
 }());
